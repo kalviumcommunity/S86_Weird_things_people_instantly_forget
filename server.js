4412bdeabe3f63fb411159ad06669291a85c6ae8
PV SONALI <pv.sonali.official@gmail.com>
2025-03-11 11:57:01 +0530

    Added MongoDB Connection

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGO_URI, )
     console.error("Failed to connect to MongoDB:", err);
   });
 
+  
 // Define a simple /ping route
 app.get("/ping", (req, res) => {
   res.json({ message: "Pong! Server is running." });
