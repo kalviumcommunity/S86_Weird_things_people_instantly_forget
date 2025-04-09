a89658018bcf63fcfff9d1108c35184470bf6be7
PV SONALI <pv.sonali.official@gmail.com>
2025-04-09 15:11:16 +0530

    setting react app in fronted folder

--- backend/controllers/itemcontroller.js
+++ backend/controllers/itemcontroller.js
@@ -1,4 +1,4 @@
-const Item = require("../models/item");
+const Item = require("../../models/item");
 
 // CREATE
 exports.createItem = async (req, res) => {
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to the "Weird Things People Instantly Forget" App 🚀</h1>
+      <p>This app highlights quirky things people often forget, and lets users interact with them!</p>
+    </div>
+  );
+}
+
+export default App;
